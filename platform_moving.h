b3433fc80a08f1421ac7c691233d0b5278b815f1
fruitsamurai-png <64308011+fruitsamurai-png@users.noreply.github.com>
2020-11-20 16:15:37 +0800

    Delete platform_moving.h

--- platform_moving.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-typedef struct plat_moving {
-	float dimx, dimy;
-	int time, speed;
-}plat_moving;
-plat_moving plats_moving[10];
-void platform_moving_init(void);
-void platform_moving_update(int i);
-void platform_moving_exit(void);
