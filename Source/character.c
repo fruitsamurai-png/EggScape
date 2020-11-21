0e8225bfb89c00df82761acb7da7e85f07ff266e
fruitsamurai-png <64308011+fruitsamurai-png@users.noreply.github.com>
2020-11-21 15:58:02 +0800

    Add files via upload

--- Source/character.c
+++ Source/character.c
@@ -7,7 +7,36 @@
 CP_Image eggy1=NULL;
 int check=0;
 float maxspeed = 0;
-void eggjump(void)
+int timer = 0;
+int alpha = 0;
+static void doublejump(void)
+{
+	CP_Settings_Fill(CP_Color_Create(255, 255, 255, 255));
+	CP_Graphics_DrawRect(windowx * 3 / 4, 0, (float)alpha, 40);
+	CP_Settings_Fill(CP_Color_Create(255, 255, 255, 255));
+	CP_Font_DrawText("Doublejump:", 480, 20);
+	if (CP_Input_KeyDown(KEY_SPACE))
+		{
+			if (egg.cooldown == 0)
+			{
+				egg.isjump = 1;
+				egg.cooldown = 1;
+				alpha = 0;
+			}
+			
+		}
+	if (egg.cooldown)
+	{
+		timer++;
+		if(alpha!=200)alpha = (int)(timer+CP_System_GetDt());
+		if (alpha >= 200)
+		{
+			egg.cooldown = 0;
+			timer = 0;
+		}
+	}
+}
+static void eggjump(void)
 {
 	//mode1(normal Jump)
 	float G = 10 * .2f;//gravity for the egg
@@ -17,7 +46,6 @@ void eggjump(void)
 			egg.h = 8;
 		egg.h += 1.f * G;//velocity of the egg
 		egg.y += .5f * egg.h;
-		egg.drop = egg.y + egg.h;
 	}
 
 	if (egg.isjump)
@@ -53,57 +81,38 @@ void eggs_init(void)
 	egg.h = 0;
 	egg.x = plats[7].dimx+(0.5f*dimw);
 	egg.y = windowy / 2;
-	egg.start = 0;
 	egg.count = 0;
 	maxspeed = 30;
+	egg.cooldown = 0;
+	alpha = 200;
 }
 void eggs_update(void)
 {
 	eggy1= CP_Image_Load("./Assets/eggy_RRUN.png");
-	if (egg.start)
-	{
+	
 		if ((CP_Input_KeyDown(KEY_D) || CP_Input_KeyDown(KEY_RIGHT)))
 		{
-			if (check == 0)
-				egg.movement = 0;
+			if (check == 0)egg.movement = 0;
 			egg.movement += (egg.maxaccel * CP_System_GetDt());
 			check = 1;
 		}
 		if ((CP_Input_KeyDown(KEY_A) || CP_Input_KeyDown(KEY_LEFT)))
 		{
-			if (check == 1)
-				egg.movement = 0;
+			if (check == 1)egg.movement = 0;
 			egg.movement -= (egg.maxaccel * CP_System_GetDt());
 			check = 0;
 		}
 		if (check == 1)
 		{
 			eggy1 = CP_Image_Load("./Assets/eggy_RRUN.png");
-			/*if (check == 0)
-			{
-				egg.movement = 0;
-			}*/
+	
 		}
 		else if (check == 0)
 		{
 			eggy1 = CP_Image_Load("./Assets/eggy_LRUN.png");
-			/*if (check == 1)
-			{
-				egg.movement = 0;
-			}*/
+			
 		}
-	}
-	if (egg.start == 2)
-	{
-		if (CP_Input_KeyDown(KEY_SPACE))
-		{
-			if (egg.jjump == 0)
-			{
-				egg.isjump = 1;
-				++egg.jjump;
-			}
-		}
-	}
+	
 	egg.x += egg.movement;
 	if (egg.movement >= maxspeed)
 	{
@@ -114,6 +123,7 @@ void eggs_update(void)
 		egg.movement = -maxspeed;
 	}
 	eggjump();// egg jumping 
+	doublejump();//double ability
 	CP_Settings_Fill(CP_Color_Create(255, 255, 255, 255));
 	CP_Image_Draw(eggy1, (float)egg.x, (float)egg.y, 100, 100, 255);
 }
@@ -125,7 +135,8 @@ void eggs_exit(void)
 	egg.isjump = 0;
 	egg.h = 0;
 	egg.movement = 0;
-	egg.start = 0;
 	egg.count = 0;
+	timer = 0;
+	alpha = 0;
 	CP_Image_Free(&eggy1);
 }
