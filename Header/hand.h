67195087cc93f2a45de2ce9f96b0f354ccf137d4
phillip28749 <73105732+phillip28749@users.noreply.github.com>
2020-11-23 15:25:19 +0800

    Add files via upload

--- Header/hand.h
+++ Header/hand.h
@@ -9,7 +9,12 @@ struct hands
 	int speed;
 	int increment;
 }hands;
+struct mouse
+{
+	float x, y,movement,speed;
+}mouse;
 struct hands hand_full;
+struct mouse mouse;
 #define attackinter 120
 void hand_init(void);
 void hand_update(void);
@@ -17,3 +22,5 @@ void hand_exit(void);
 static void raise_hands(void);
 void hands_movement(void);
 void hand_Collision(void);
+void mouse_init(void);
+void mouse_drop(void);
