cebb7dbbec57d4d1177b17b68216301571677481
fruitsamurai-png <64308011+fruitsamurai-png@users.noreply.github.com>
2020-11-23 15:23:43 +0800

    Add files via upload

--- Header/hand.h
+++ Header/hand.h
@@ -9,12 +9,7 @@ struct hands
 	int speed;
 	int increment;
 }hands;
-struct mouse
-{
-	float x, y,movement,speed;
-}mouse;
 struct hands hand_full;
-struct mouse mouse;
 #define attackinter 120
 void hand_init(void);
 void hand_update(void);
@@ -22,5 +17,3 @@ void hand_exit(void);
 static void raise_hands(void);
 void hands_movement(void);
 void hand_Collision(void);
-void mouse_init(void);
-void mouse_drop(void);
