1cc094b9ec22ef914bc1c41e3fca7745263f3ffe
fruitsamurai-png <64308011+fruitsamurai-png@users.noreply.github.com>
2020-12-04 20:24:46 +0800

    Add files via upload

--- Header/rat.h
+++ Header/rat.h
@@ -3,7 +3,7 @@ struct mouse
 {
 	float x, y, movement, speed,alpha;
 }mouse;
-struct mouse mouses[10];
+struct mouse mouses[2];
 void mouse_init(size_t count1);
 void mouse_update(size_t count3);
 void mouse_exit(size_t count2);
